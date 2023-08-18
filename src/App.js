5f89b88933987d869556dd521ad4fb08a8f57349
WagnoLeaoSergio <wagnoleao@gmail.com>
2023-08-18 11:45:52 -0300

    primeiro commit, estrutura da pagina implementada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import codiLogo from './logo_codi_academy.png'
+
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <img src={codiLogo} className="codi-logo" alt="logo" />
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <div className="buttons">
+      <div className="quiz-progress">
+        10%
+      </div>
+      <button>Próxima</button>
+    </div>
+  )
+}
+
+function QuestionPanel() {
+  return (
+    <div className="question-box codi-box-style">
+      <div className="quiz-stats">
+        <div className="quiz-number-questions">
+          1/10
+        </div>
+      </div>
+      <Question />
+      <Footer />
+    </div>
+
+  )
+}
+
+function Question() {
+  return (
+    <div className="question">
+      <h3>Pergunta 1:</h3>
+      <h2>Qual é o animal do símbolo da linguagem GoLang?</h2>
+      <ul className="question-answers">
+        <li className="codi-box-style">Gato</li>
+        <li className="codi-box-style">Cachorro</li>
+        <li className="codi-box-style">Macaco</li>
+        <li className="codi-box-style">Hamster</li>
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="quiz-window">
+        <h1>Questionário de Tecnologia</h1>
+        <QuestionPanel />
+      </div>
+    </>
   );
 }
 
